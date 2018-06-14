ff0a3bb98638f440a1013ed0ed8a239dc7abf858
junhong <junhong@wacai.com>
2018-06-14 22:29:33 +0800

    add springboot

---
+++ simple-springboot/src/main/java/com/demo/Bean1.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+/**
+ * @author junhong
+ */
+public class Bean1 {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ simple-springboot/src/main/java/com/demo/StudentService.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+/**
+ * @author junhong
+ */
+public class StudentService {
+
+    public String getName(Student student) {
+        return student.getName();
+    }
+
+    public int getAge(Student student) {
+        return student.getAge();
+    }
+
+}
